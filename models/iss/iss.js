3844333b0fda89423f1ceb25a7694944e763f3f5
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-10-01 23:30:41 +0200

    Add ISS location in real time

--- models/iss/iss.js
+++ models/iss/iss.js
@@ -1,6 +1,8 @@
 import scene from '../../app/scene.js';
 import {GLTFLoader} from '/jsm/loaders/GLTFLoader.js';
 import {DRACOLoader} from '/jsm/loaders/DRACOLoader.js';
+import getCoordinates from './location.js';
+import getRenderCoordinates from '../earth/location.js';
 
 const ISSGltf = "models/iss/gltf/iss.gltf";
 const loader = new GLTFLoader();
@@ -10,12 +12,21 @@ loader.setDRACOLoader( dracoLoader );
 
 function loadISS() {
   loader.load(ISSGltf, (gltf) => {
-    gltf.scene.position.x = 0.8;
-    gltf.scene.scale.set(0.01, 0.01, 0.01);
-    setISS(gltf.scene);
+    renderISS(gltf);
   }, undefined, (error) => {
     console.error(error);
   });
+}
+
+async function renderISS(gltf) {
+  const ISSCoordinates = await getCoordinates();
+  const ISSCoordinatesInEarth = getRenderCoordinates(ISSCoordinates);
+  gltf.scene.rotation.z = -1.5;
+  gltf.scene.position.x = ISSCoordinatesInEarth.x;
+  gltf.scene.position.y = ISSCoordinatesInEarth.y;
+  gltf.scene.scale.set(0.01, 0.01, 0.01);
+  setISS(gltf.scene);
+  setTimeout(() => scene.remove(gltf.scene), 2000);
 }
 
 function setISS(mesh) {
@@ -23,4 +34,13 @@ function setISS(mesh) {
 }
 
 
-export default loadISS;
+function renderISSEachSecond() {
+  setTimeout(() => continuouslyRenderISS(), 2000);
+}
+
+function continuouslyRenderISS() {
+  renderISSEachSecond();
+  loadISS();
+}
+
+export default renderISSEachSecond;
