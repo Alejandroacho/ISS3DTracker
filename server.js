3461a18b2cb80ed8ee76714708a9cf16003716ba
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-10-02 15:37:09 +0200

    Remove unnused things

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const path = require('path');
 app.use(express.static(__dirname))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+app.use('/js/', express.static(path.join(__dirname, 'node_modules/three/examples/js')));
 app.use('/libs/', express.static(path.join(__dirname, 'node_modules/three/examples/js/libs')));
+app.use('/addons/', express.static(path.join(__dirname, 'node_modules/three/addons')));
 
 app.listen(8000, () =>
   console.log('Visit http://127.0.0.1:8000')
