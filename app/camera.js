b3a79b6b8e365a231feee4a2b450e5504894264b
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-10-02 12:39:36 +0200

    Refactor camera and controls settings

--- app/camera.js
+++ app/camera.js
@@ -2,9 +2,9 @@ import * as THREE from '/build/three.module.js';
 
 const fov = 60;
 const aspect = window.innerWidth / window.innerHeight;
-const near = 0.1;
+const near = 0.009;
 const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 5;
 
 export default camera;
